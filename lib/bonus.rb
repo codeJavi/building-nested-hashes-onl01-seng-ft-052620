e76e7a9ff91ec049332b946c428e3b9f2c0d576c
Javi Leyva <jvrleyva@gmail.com>
2020-06-03 15:59:53 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -26,5 +26,7 @@ epic_tragedy[:montague][:hero][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+    epic_tragedy
+end
   epic_tragedy
 end
